be04fa25850711f231d544a4ff3d302b5fb33205
LeonardoAlmd <leonardoalmd8@outlook.com>
2022-12-02 01:06:10 -0300

    Refatoração do Codigo até o inicio do box-cursos.

---
+++ script.js
@@ -0,0 +1,33 @@
+const navEl = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 56) {
+        navEl.classList.add('navbar-scrolled');
+    }else if (window.scrollY < 56) {
+        navEl.classList.remove('navbar-scrolled');
+    }
+});
+
+const menuLinks = document.querySelectorAll('.navbar a[href^="#"]');
+
+function getDistanceFromTheTop(element){
+    const id = element.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+}
+
+function nativeScroll(getDistanceFromTheTop) {
+    window.scroll({
+        top: getDistanceFromTheTop,
+        behavior: "smooth"
+    })
+}
+
+function scrollToSection(event){
+    event.preventDefault();
+    const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
+    nativeScroll(distanceFromTheTop)
+}
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+});
